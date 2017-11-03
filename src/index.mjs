8fa1a4e57e6e706f228c597e86c271c8a2a151cb
Jonathan Grimes <jsg2021@gmail.com>
2017-11-03 15:02:52 -0500

    without http server stuff

--- src/index.mjs
+++ src/index.mjs
@@ -1,33 +1,3 @@
-import express from 'express';
-import morgan from 'morgan';
-import responseTime from 'response-time';
+//use esm "import" syntax here...
 
-import cors from './middlewares/cors';
-import caching from './middlewares/caching';
-
-const app = express();
-
-app.use(responseTime());
-app.use(morgan('combined'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(cors);
-app.use(caching);
-
-app.get('/', async (req, res) => {
-
-	const result = await someLongAsyncProcess();
-
-	res.send(`Hello World! With: ${result}`);
-});
-
-
-app.listen(3000, () => {
-	console.log('Listening on port 3000!');
-});
-
-
-
-async function someLongAsyncProcess () {
-	return await new Promise(f => setTimeout(() => f('data'), 1000));
-}
+console.log('Hello World!');
