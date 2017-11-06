1c4a281b8640345c7966c7387582109478684633
Jonathan Grimes <jsg2021@users.noreply.github.com>
2017-11-06 14:48:44 -0600

    better cache headers

--- src/middlewares/caching.mjs
+++ src/middlewares/caching.mjs
@@ -1,9 +1,12 @@
 export default function (_, res, next) {
 	if (!res.headersSent) {
-		res.setHeader('Cache-Control',	'private, no-cache, no-store, ' +
-										'must-revalidate, max-stale=0, ' +
-										'post-check=0, pre-check=0');
-		res.setHeader('Expires', '-1'); //old, but still arounad :(
+		res.setHeader('Cache-Control', [
+			'no-cache',
+			'no-store',
+			'must-revalidate',
+			'max-age=0'
+		].join(', '));
+		res.setHeader('Expires', 'Thu, 01 Jan 1970 00:00:00 GMT'); //old, but still arounad :(
 		res.setHeader('Pragma', 'no-cache'); //old, but still arounad :(
 	}
 	next();
