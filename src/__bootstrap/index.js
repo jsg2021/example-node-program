bd41005ba548bce550344af9016cbdf2c9ce776d
Jonathan Grimes <jsg2021@gmail.com>
2018-04-04 16:09:20 -0500

    update deps

--- src/__bootstrap/index.js
+++ src/__bootstrap/index.js
@@ -1,4 +1,4 @@
 /*eslint import/no-commonjs: 0, strict: 0*/
 'use strict';
 require('./polyfills');
-require('@std/esm')(module)('../index.mjs');
+require('esm')(module)('../index.mjs');
--- src/__bootstrap/polyfills.js
+++ src/__bootstrap/polyfills.js
@@ -3,6 +3,5 @@
 (() => {//iife for istanbul ignore next
 	//add polyfills and shims
 	// require('core-js');
-	// require('regenerator-runtime/runtime');
 	global.fetch = global.fetch || require('node-fetch');
 })();
